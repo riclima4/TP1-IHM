10ee6879ced8f3867070f0f3674b299c90018f3d
Ricardo Lima <ric.lima1@hotmail.com>
2025-03-31 22:33:49 +0100

    home page create

---
+++ src/app/data/pointsOfInterestDatasource.ts
@@ -0,0 +1,121 @@
+export const pointsOfInterestDatasource = [
+  {
+    "tipo": "monumento",
+    "nome": "Santuário de Santa Luzia",
+    "descricao": "Vista panorâmica incrível sobre a cidade.",
+    "localizacao": "Santa Luzia",
+    "foto": null
+  },
+  {
+    "tipo": "monumento",
+    "nome": "Navio-Hospital Gil Eannes",
+    "descricao": "Museu flutuante sobre a frota bacalhoeira.",
+    "localizacao": "Doca Comercial",
+    "foto": null
+  },
+  {
+    "tipo": "monumento",
+    "nome": "Sé Catedral de Viana do Castelo",
+    "descricao": "Igreja Matriz com arquitetura românica.",
+    "localizacao": "Centro Histórico",
+    "foto": null
+  },
+  {
+    "tipo": "monumento",
+    "nome": "Chafariz da Praça da República",
+    "descricao": "Monumento renascentista no centro histórico.",
+    "localizacao": "Praça da República",
+    "foto": null
+  },
+  {
+    "tipo": "monumento",
+    "nome": "Citânia de Santa Luzia",
+    "descricao": "Ruínas de um antigo povoado celta.",
+    "localizacao": "Santa Luzia",
+    "foto": null
+  },
+  {
+    "tipo": "monumento",
+    "nome": "Forte de Santiago da Barra",
+    "descricao": "Antiga fortaleza junto ao porto.",
+    "localizacao": "Avenida Campo do Castelo",
+    "foto": null
+  },
+  {
+    "tipo": "praia",
+    "nome": "Praia do Cabedelo",
+    "descricao": "Ideal para surf e kitesurf.",
+    "localizacao": "Margem Sul do Rio Lima",
+    "foto": null
+  },
+  {
+    "tipo": "praia",
+    "nome": "Praia Norte",
+    "descricao": "Perfeita para passeios à beira-mar.",
+    "localizacao": "Margem Norte do Rio Lima",
+    "foto": null
+  },
+  {
+    "tipo": "museu",
+    "nome": "Museu do Traje",
+    "descricao": "Trajes típicos da região minhota.",
+    "localizacao": "Centro Histórico",
+    "foto": null
+  },
+  {
+    "tipo": "museu",
+    "nome": "Museu de Artes Decorativas",
+    "descricao": "Artes e mobiliário dos séculos XVII e XVIII.",
+    "localizacao": "Rua Góis Pinto",
+    "foto": null
+  },
+  {
+    "tipo": "museu",
+    "nome": "Centro de Mar",
+    "descricao": "História marítima da cidade.",
+    "localizacao": "Doca Comercial",
+    "foto": null
+  },
+  {
+    "tipo": "restaurante",
+    "nome": "O Manel",
+    "especialidade": "Mariscos e peixe fresco.",
+    "localizacao": "Rua da Bandeira",
+    "foto": null
+  },
+  {
+    "tipo": "restaurante",
+    "nome": "Tasquinha da Linda",
+    "especialidade": "Peixe e marisco de qualidade.",
+    "localizacao": "Avenida do Cabedelo",
+    "foto": null
+  },
+  {
+    "tipo": "restaurante",
+    "nome": "Camelo Restaurante",
+    "especialidade": "Gastronomia minhota tradicional.",
+    "localizacao": "Rua do Poço",
+    "foto": null
+  },
+  {
+    "tipo": "restaurante",
+    "nome": "Casa Primavera – O Forno",
+    "especialidade": "Polvo à lagareiro.",
+    "localizacao": "Rua Manuel Espregueira",
+    "foto": null
+  },
+  {
+    "tipo": "restaurante",
+    "nome": "Maria Petisca",
+    "especialidade": "Petiscos e ambiente acolhedor.",
+    "localizacao": "Largo João Tomás da Costa",
+    "foto": null
+  },
+  {
+    "tipo": "restaurante",
+    "nome": "Foz do Minho",
+    "especialidade": "Vista espetacular sobre o rio Minho.",
+    "localizacao": "Caminha",
+    "foto": null
+  }
+]
--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { pointsOfInterestDatasource } from 'src/app/data/pointsOfInterestDatasource';
 
 @Component({
   selector: 'app-home',
@@ -9,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 export class HomePage implements OnInit {
   title: string = 'Início';
 
+  pointsOfInterestData: any[] = pointsOfInterestDatasource;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  goToLendasPDF() {
+    window.open('https://www.cm-viana-castelo.pt/wp-content/uploads/2022/11/lendas_de_viana_do_castelo.pdf', '_blank');
+  }
 }
