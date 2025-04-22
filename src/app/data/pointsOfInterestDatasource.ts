b7c4ad8a058728f7969d2c93b9f83087cd5f8340
Ricardo Lima <ric.lima1@hotmail.com>
2025-04-22 21:22:43 +0100

    poi finish

--- src/app/data/pointsOfInterestDatasource.ts
+++ src/app/data/pointsOfInterestDatasource.ts
@@ -7,8 +7,8 @@ export const pointsOfInterestDatasource = [
     "coordenadas": "41.70188767982144, -8.834201395943657",
     "foto": "//upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Santa_Luzia_Church%2C_Viana_do_Castelo%2C_Portugal.jpg/250px-Santa_Luzia_Church%2C_Viana_do_Castelo%2C_Portugal.jpg",
     "portfolio": [
-      "https://upload.wikimedia.org/wikipedia/commons/2/2a/Santu%C3%A1rio_de_Santa_Luzia_1.jpg",
-      "https://upload.wikimedia.org/wikipedia/commons/4/42/Vista_do_Santu%C3%A1rio_de_Santa_Luzia.jpg"
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0f/44/5a/e5/santa-luzia-plus-small.jpg?w=900&h=500&s=1",
+      "https://upload.wikimedia.org/wikipedia/commons/9/9d/Viana_do_Castelo-Santa_Luzia_altar.jpg"
     ],
     "historia": "Construído no início do século XX, o Santuário de Santa Luzia é inspirado na Basílica de Sacré-Cœur, em Paris. É um dos marcos mais icônicos de Viana do Castelo, atraindo visitantes pela sua arquitetura neobizantina e pela vista deslumbrante do topo do monte."
   },
@@ -20,8 +20,8 @@ export const pointsOfInterestDatasource = [
     "coordenadas": "41.69043590283794, -8.82947534366906",
     "foto": "//upload.wikimedia.org/wikipedia/commons/thumb/9/94/Viana_do_Castelo_-_panoramio_%2862%29.jpg/300px-Viana_do_Castelo_-_panoramio_%2862%29.jpg",
     "portfolio": [
-      "https://upload.wikimedia.org/wikipedia/commons/3/34/Navio_Gil_Eannes_Interior.jpg",
-      "https://upload.wikimedia.org/wikipedia/commons/8/8a/Navio_Gil_Eannes.jpg"
+      "https://visitasdeestudo.pt/wp-content/uploads/2020/01/Navio-Hospital-Gil-Eanes-01.jpg",
+      "https://turistaslupa.com/wp-content/uploads/2021/01/20200811_151201.jpg?w=1024"
     ],
     "historia": "O Navio-Hospital Gil Eannes foi construído em 1955 para apoiar a frota pesqueira portuguesa no Atlântico Norte. Após anos de serviço, foi restaurado e transformado em museu, preservando a memória da pesca do bacalhau e da vida marítima portuguesa."
   },
@@ -33,8 +33,8 @@ export const pointsOfInterestDatasource = [
     "coordenadas": "41.69337946573814, -8.82699648270366",
     "foto": "//upload.wikimedia.org/wikipedia/commons/thumb/0/0e/Viana_do_Castelo_-_panoramio_%2859%29_%28cropped%29.jpg/250px-Viana_do_Castelo_-_panoramio_%2859%29_%28cropped%29.jpg",
     "portfolio": [
-      "https://upload.wikimedia.org/wikipedia/commons/b/b7/S%C3%A9_de_Viana_do_Castelo.jpg",
-      "https://upload.wikimedia.org/wikipedia/commons/5/5b/Interior_S%C3%A9_Viana_do_Castelo.jpg"
+      "https://euroveloportugal.com/files/2016/07/a9_felix_iglesias_se_catedral_vc.jpg",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSy6c-UpxqwMPPWunQ14z9h6tMfUtazNOv7Xg&s"
     ],
     "historia": "Datada do século XV, a Sé Catedral de Viana do Castelo é um exemplo notável da arquitetura religiosa medieval. Com elementos românicos e góticos, a catedral é um marco histórico e cultural da cidade."
   },
@@ -46,8 +46,8 @@ export const pointsOfInterestDatasource = [
     "coordenadas": "41.69472204624847, -8.848909145265322",
     "foto": "https://cdn.visitportugal.com/sites/default/files/styles/encontre_detalhe_poi_destaque/public/mediateca/N4.PRA1620D.jpg?itok=oMhBDPgk",
     "portfolio": [
-      "https://upload.wikimedia.org/wikipedia/commons/6/67/Praia_Norte_Viana_do_Castelo.jpg",
-      "https://upload.wikimedia.org/wikipedia/commons/d/d1/Por_do_sol_Praia_Norte_Viana.jpg"
+      "https://cdn.visitportugal.com/sites/default/files/styles/encontre_detalhe_poi_destaque/public/mediateca/N4.PRA1620D.jpg?itok=oMhBDPgk",
+      "https://www.playocean.net/i/portugal/beaches/viana-do-castelo/norte/praia-do-norte-4.jpg"
     ],
     "historia": "A Praia Norte é conhecida por sua beleza natural e tranquilidade. Ao longo dos anos, tornou-se um destino popular para relaxamento, caminhadas e esportes aquáticos, sendo um ponto de encontro para moradores e turistas."
   },
@@ -59,8 +59,8 @@ export const pointsOfInterestDatasource = [
     "coordenadas": "41.70310123215079, -8.822708142720032",
     "foto": "https://lh3.googleusercontent.com/p/AF1QipN6zew-dOyJQVLC29-9Asc2YiXHW606FKtM8oQ=s680-w680-h510",
     "portfolio": [
-      "https://upload.wikimedia.org/wikipedia/commons/3/3d/Prato_de_Marisco.jpg",
-      "https://upload.wikimedia.org/wikipedia/commons/9/94/Vista_do_restaurante_O_Manel.jpg"
+      "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2d/99/49/2d/restaurante-o-manel.jpg?w=900&h=500&s=1",
+      "https://www.restauranteomanel.pt/wp-content/uploads/2016/06/02.jpg"
     ],
     "historia": "Fundado há décadas, o restaurante 'O Manel' é um ícone da gastronomia local. Reconhecido pela qualidade dos seus mariscos e peixes frescos, é um ponto de referência para quem deseja experimentar a culinária tradicional portuguesa."
   }
