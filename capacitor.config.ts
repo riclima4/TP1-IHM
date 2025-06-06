743f0d3f350bc71cbd9560218dd338b60ed8c708
riclima4 <ric.lima1@hotmail.com>
2025-06-06 21:48:53 +0100

    .

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.vianaapp.ihm',
+  appId: 'app.viana.ihm',
   appName: 'IHM-Viana',
   webDir: 'www'
 };
