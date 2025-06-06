ee25a9215c9eb27189276597201b3d77ac2a0b17
riclima4 <ric.lima1@hotmail.com>
2025-06-06 22:02:23 +0100

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'app.viana.ihm',
   appName: 'IHM-Viana',
-  webDir: 'www'
+  webDir: 'www',
+  android: {
+    adjustMarginsForEdgeToEdge: 'force'
+  }
 };
 
 export default config;
